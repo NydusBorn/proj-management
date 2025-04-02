e3c50e90298422ccf49a3d4f70878a9293a035e5
NydusBorn <nydusborn@outlook.com>
2025-04-02 21:29:39 +0300

    make output use all 3 algorythms

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import grid
 import solvers
-
 if __name__ == "__main__":
     grid_cur = grid.grid(45, 135)
     grid_cur.carve_passages_from(1, 1)
+    solution = solvers.BFS(grid_cur)
+    print("BFS")
+    print(f"Path length: {solution["length"]}")
+    print(f"Time spent: {solution["time"]}s")
+    print(grid_cur.__str__())
+    grid_cur.clear_path()
     solution = solvers.dfs_solution(grid_cur)
+    print("DFS")
     print(f"Path length: {solution[0]}")
     print(f"Time spent: {solution[1]}s")
     print(grid_cur.__str__())
     grid_cur.clear_path()
-    grid_cur = grid.grid(45, 131)
-    grid_cur.carve_passages_from(1,1)
-
     solution = solvers.greedy_solution(grid_cur)
+    print("Greedy")
     print(f"Path length: {solution[0]}")
     print(f"Time spent: {solution[1]}s")
     print(grid_cur.__str__())
-    grid_cur.clear_path()
-    print(grid_cur.__str__())
-
 
