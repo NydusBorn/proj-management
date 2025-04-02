3be7bf12dfe41438287bb27adb403a2cbfb98000
milana-cat <103025089+milana-cat@users.noreply.github.com>
2025-04-02 20:54:28 +0300

    final

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import grid
 
+import solvers
 if __name__ == "__main__":
-    grid_cur = grid.grid(45, 131)
-    grid_cur.carve_passages_from(1,1)
+    grid_cur = grid.grid(45, 135)
+    grid_cur.carve_passages_from(1, 1)
+    solution = solvers.dfs_solution(grid_cur)
+    print(f"Path length: {solution[0]}")
+    print(f"Time spent: {solution[1]}s")
     print(grid_cur.__str__())
