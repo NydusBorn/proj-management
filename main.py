fb701c631729f1d228c3bc99ae883984eb18920e
NydusBorn <nydusborn@outlook.com>
2025-04-02 20:15:17 +0300

    Create solvers.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from xml.etree.ElementTree import tostring
-
 import grid
 
 if __name__ == "__main__":
-    grid_cur = grid.grid(11, 11)
+    grid_cur = grid.grid(45, 131)
     grid_cur.carve_passages_from(1,1)
     print(grid_cur.__str__())
